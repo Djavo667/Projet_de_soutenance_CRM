29e0053dcf31e3b960ac9aaed03c63baf4359341
Djavo667 <djavojunior@gamil.com>
2026-04-13 19:18:45 +0000

    modification du projet CRM

--- LE_CRM/admin.py
+++ LE_CRM/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+class clientAdmin(admin.ModelAdmin):
+    # Colonnes affichées dans la liste
+    list_display = ("nom", "prenom", "sexe", "email", "telephone", "actif" , "date_enregistrement")
+    # Filtres dans la barre latérale
+    search_fields = ("nom", "prenom", "email", "telephone")
+    # Filtre dans la barre latérale
+    list_filter = ("sexe", "actif", "date_enregistrement")
+    # Titres des sections
+    fieldsets = (
+        ('Informations principales', {
+            'fields': ('nom', 'prenom', 'telephone')
+        }),
+        ('Contact', {
+            'fields': ('email',),
+            'classes': ('collapse',)
+        }),
+        ('Statut', {
+            'fields': ('actif', 'date_enregistrement'),
+            'classes': ('collapse',)
+        }),
+    )
--- LE_CRM/models.py
+++ LE_CRM/models.py
@@ -1,11 +1,26 @@
+from django.utils import timezone
+
 from django.db import models
 
 # Create your models here.
 class client(models.Model):
-    nom = models.CharField(max_length=100)
-    prenom = models.CharField(max_length=100)
-    sexe = models.CharField(max_length=1, choices=[('M', 'Masculin'), ('F', 'Féminin')])
-    email = models.EmailField()
-    telephone = models.CharField(max_length=20)
-    adresse = models.CharField(max_length=200)
+    nom = models.CharField(max_length=100, verbose_name="Nom")
+    prenom = models.CharField(max_length=100, verbose_name="Prénom")
+    sexe = models.CharField(max_length=1, choices=[('M', 'Masculin'), ('F', 'Féminin')], verbose_name="Sexe")
+    email = models.EmailField(verbose_name="Email")
+    telephone = models.CharField(max_length=20, verbose_name="Téléphone")
+    adresse = models.CharField(max_length=200, verbose_name="Adresse")
     description = models.TextField()
+    date_enregistrement = models.DateTimeField(
+        default=timezone.now, 
+        verbose_name="Date d'enregistrement"
+    )
+    actif = models.BooleanField(default=True, verbose_name="Actif")
+
+    class Meta:
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
+        ordering = ['prenom',"nom"]
+        unique_together = ('telephone', 'email')
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
